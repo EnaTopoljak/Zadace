b6cc97b1890b07e22dbdb934917c8a538cafa609
enatopoljak <ena.t92@gmail.com>
2016-11-03 19:47:20 +0100

    Zadatak 13 Animations

--- Zadatak13Animations/Zadatak13Animations/Zadatak13Animations/ViewController.swift
+++ Zadatak13Animations/Zadatak13Animations/Zadatak13Animations/ViewController.swift
@@ -15,18 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var innerGreenSquare: UIView!
     @IBOutlet weak var innerOrangeSquare: UIView!
     @IBOutlet weak var redSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        outerGreenSquare.backgroundColor = UIColor.init(red:0.02, green:0.39, blue:0.09, alpha:1.0)
-        outerOrangeSquare.backgroundColor = UIColor.init(red:1.00, green:0.64, blue:0.26, alpha:0.8)
+        outerGreenSquare.backgroundColor = UIColor.init(red:0.35, green:0.59, blue:0.20, alpha:1.0)
+        outerOrangeSquare.backgroundColor = UIColor.init(red:1.00, green:0.64, blue:0.26, alpha:0.7)
         innerGreenSquare.backgroundColor = UIColor.init(red:0.60, green:0.63, blue:0.21, alpha:0.9)
         innerOrangeSquare.backgroundColor = UIColor.init(red:1.00, green:0.56, blue:0.00, alpha:1.0)
-        redSquare.backgroundColor = UIColor.init(red:1.00, green:0.00, blue:0.00, alpha:0.5)
-        
-        //redSquare.frame.size.height = 100
-        //redSquare.frame.size.width = 100
-        
-            }
+        redSquare.backgroundColor = UIColor.init(red:1.00, green:0.00, blue:0.00, alpha:0.6)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -35,32 +32,29 @@ class ViewController: UIViewController {
 
     @IBAction func playAnimation(_ sender: UIBarButtonItem) {
         
-        UIView.animate(withDuration: 1, delay: 0, animations: {
+        UIView.animate(withDuration: 0.65, delay: 0, animations: {
             self.outerOrangeSquare.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
         }, completion: {_ in
-            UIView.animate(withDuration: 1, delay: 0, animations: {
+            UIView.animate(withDuration: 1.3, delay: 0, animations: {
                 self.outerOrangeSquare.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)}, completion: nil)})
         
-        UIView.animate(withDuration: 1, delay: 0, animations: {
-            self.innerGreenSquare.transform = CGAffineTransform(scaleX: 0.55, y: 0.55)
+        UIView.animate(withDuration: 1.1, delay: 0, animations: {
+            self.innerGreenSquare.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
         }, completion: {_ in
-            UIView.animate(withDuration: 1, delay: 0, animations: {
+            UIView.animate(withDuration: 0.1, delay: 0, animations: {
                 self.innerGreenSquare.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)}, completion: nil)})
         
-        UIView.animate(withDuration: 1.0, delay: 0, animations: {
+        UIView.animate(withDuration: 1.0, delay: 0.1, animations: {
             self.innerOrangeSquare.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
         }, completion: {_ in
-            UIView.animate(withDuration: 1.0, delay: 0, animations: {
+            UIView.animate(withDuration: 0.25, delay: 0.0, animations: {
                 self.innerOrangeSquare.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)}, completion: nil)})
         
-        UIView.animate(withDuration: 1.0, delay: 0, animations: {
-            self.redSquare.transform = CGAffineTransform(scaleX: 9.0, y: 9.0)
+        UIView.animate(withDuration: 0.8, delay: 0.1, animations: {
+            self.redSquare.transform = CGAffineTransform(scaleX: 5.7, y: 5.7)
         }, completion: { _ in
-            UIView.animate(withDuration: 0.9, delay: 0, animations: {
-                self.redSquare.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)}, completion: nil)
-            
-        } )
-    
+            UIView.animate(withDuration: 0.5, delay: 0, animations: {
+                self.redSquare.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)}, completion: nil)})
     }
 }
 
